23e9738639410f10cc69ccfb67d4372059ae5928
Gregory Casamento <greg.casamento@gmail.com>
2020-11-08 09:00:15 -0500

    Add all files

---
+++ findmultiple.rb
@@ -0,0 +1,15 @@
+def findmultiple(array)
+  result = {}
+  array.each do |n|
+    result[n] = 0 if result[n].nil?
+    result[n] = result[n] + 1 if result[n].nil? == false
+  end
+
+  result.keys.each do |k|
+    result.delete(k) if result[k] == 1
+  end
+  result
+end
+
+r = findmultiple([1,1,2,2,3,3,4,4,5,5])
+puts r
